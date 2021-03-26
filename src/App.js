09ca89662de7457d0b319f5d147aab59de901b86
Syahrizal Setiawan <syahrizal.setiawan11@gmail.com>
2021-03-26 11:15:22 +0700

    lazyload

--- src/App.js
+++ src/App.js
@@ -1,35 +1,41 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import Navigation from './components/Navigation/Navigation'
 
 import './assets/styles/main.scss'
 
-import PokemonList from './pages/PokemonList/PokemonList'
-import MyPokemonList from './pages/MyPokemonList/MyPokemonList'
+const MyPokemonList = React.lazy(() => import('./pages/MyPokemonList/MyPokemonList'))
+const DetailedPokemon = React.lazy(() => import( './pages/Pokemon/DetailedPokemon'))
+const PokemonList = React.lazy(() => import('./pages/PokemonList/PokemonList'))
 
 import { ApolloProvider } from '@apollo/client'
 
 import { client } from './GraphQL/index'
-import DetailedPokemon from './pages/Pokemon/DetailedPokemon'
 import { ThemeContextProvider } from './context/ThemeContext'
 import { theme } from './constant/Theme'
 
-function App() {
+const Loading = () => {
+  return <span>Loading</span>
+}
+
+const App = () => {
   return (
-    <ThemeContextProvider value={theme}>
-      <ApolloProvider client={client}>
-        <Router>
-          <Navigation />
-            <Switch>
-              <Route path="/pokemon" exact component={PokemonList} />
-              <Route path="/my-pokemon" exact component={MyPokemonList} />
-              <Route path="/pokemon-detail/:id" exact component={DetailedPokemon} />
-              <Route path="/" render={() => <Redirect to='/pokemon' />} />
-              <Route component={() => 404} />
-            </Switch>
-        </Router>
-      </ApolloProvider>
-    </ThemeContextProvider>
+    <Suspense fallback={Loading()}>
+      <ThemeContextProvider value={theme}>
+        <ApolloProvider client={client}>
+          <Router>
+            <Navigation />
+              <Switch>
+                <Route path="/pokemon" exact component={PokemonList} />
+                <Route path="/my-pokemon" exact component={MyPokemonList} />
+                <Route path="/pokemon-detail/:id" exact component={DetailedPokemon} />
+                <Route path="/" render={() => <Redirect to='/pokemon' />} />
+                <Route component={() => 404} />
+              </Switch>
+          </Router>
+        </ApolloProvider>
+      </ThemeContextProvider>
+    </Suspense>
   )
 }
 
